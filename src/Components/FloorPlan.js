42131fef8fef0a413b93757f7adc3dd6862946ad
Hardikgithub2020 <hardik.unity@gmail.com>
2020-09-12 20:11:27 -0400

    Create components

---
+++ src/Components/FloorPlan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FloorPlan(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default FloorPlan;
---
+++ src/Components/Resident.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Resident(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Resident;
