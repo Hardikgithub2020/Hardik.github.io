673a9ceefe60b8ec227e953015c05c92872d4c40
Hardikgithub2020 <hardik.unity@gmail.com>
2020-09-19 21:55:38 -0400

    Debug console warning

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,43 +1,31 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React,{Component} from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
 require('dotenv').config();
+
 
  
 export class MapContainer extends Component {
   render() {
     const style = {
-        width: '90%',
-        height: '70%'
-      }
+                      width: '90%',
+                      height: '70%'
+                  }
     return (
-    
-    <div className="card-body">
-        <Map
-              google={this.props.google}
-              style={style}
-              initialCenter={{
-                lat: 39.103119 ,
-                lng: -84.512016 
-              }}
-              zoom={14}
-              onClick={this.onMapClicked}
-        >
- 
-              <Marker onClick={this.onMarkerClick} name={'RESERVATION'} />
-      
-              <InfoWindow onClose={this.onInfoWindowClose}>
-                <div>
-                    <h1>Cincinnati</h1>
-                  </div>
-              </InfoWindow>
-         </Map>
-      </div>
-    );
-    
+              <div className="card-body">
+                <Map google={this.props.google} style={style} initialCenter={{lat: 39.103119 ,lng: -84.512016 }} zoom={14} onClick={this.onMapClicked} >
+                    <Marker onClick={this.onMarkerClick} name={'RESERVATION'} />
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                          <div>
+                            <h1>Cincinnati</h1>
+                          </div>
+                    </InfoWindow>      
+                </Map>
+              </div>
+            );
   }
-
 }
  
 export default GoogleApiWrapper({
-  apiKey: process.env.GOOGLE_API_KEY
+  apiKey: ("AIzaSyB9fTXamrH7p1iSzg8sokzUF_o5euK-N58")
 })(MapContainer)
